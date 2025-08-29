91f3f247550b39805f00bc89f92936c5e37a4d65
BasselQbeisi5 <bassel.qbeisi@jeffersonrise.org>
2025-08-29 01:45:58 +0000

    add coin to collectableList

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -28,34 +28,45 @@ $(function () {
 
     // TODO 1 - Enable the Grid
 
-     toggleGrid();
+    //  toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-createPlatform(100, 700, 80, 15, "green");
+createPlatform(100, 700, 90, 15, "green");
 
-createPlatform(300, 600, 80, 15, "green");
+createPlatform(300, 600, 90, 15, "green");
 
-createPlatform(90, 550, 70, 15, "green");
+createPlatform(90, 550, 60, 15, "green");
 
-createPlatform(300, 450, 80, 15, "green");
+createPlatform(320, 450, 80, 15, "green");
 
-createPlatform(500, 350, 80, 15, "green");
+createPlatform(100, 350, 90, 15, "green");
 
-createPlatform(700, 450, 80, 15, "green");
+createPlatform(700, 350, 90, 15, "green");
+
+createPlatform(900, 450, 90, 15, "green");
+
+createPlatform(400, 250, 200, 15, "blue", 200, 500, 2)
 
     // TODO 3 - Create Collectables
 
-    createCollectable("database", 300, 550);
+    createCollectable("database", 325, 550);
+
+    createCollectable("database", 525, 170, 0.5, 1);
+
+    createCollectable("coin", 725, 300, 0, 0);    
 
 
-
-    
     // TODO 4 - Create Cannons
 
+    createCannon("top", 275, 900);
 
-    
+// createCannon("right", 350, 1500);
+
+createCannon("bottom", 550, 1500);
+
+    createCannon("bottom", 400, 1700);
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
