e2742bd0650ee17f9b2ca1c4792f21c962409cdc
BasselQbeisi5 <bassel.qbeisi@jeffersonrise.org>
2025-08-29 02:02:12 +0000

    add abood to collectableList

--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -99,4 +99,5 @@ var collectableList = {
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
  coin: { image: "images/collectables/coin.png" },
+ abood: { image: "images/collectables/abbod.png"},
 };
